ce01b796e1a53a0437dc4dc12dc5725009743bd5
Jairo Dias <jairopereira_dias@hotmail.com>
2020-08-09 18:40:46 -0300

    refatorando

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,10 +1,37 @@
 import express from 'express';
+import { promises as fs } from 'fs';
 
-const router = express.Router();
+const app = express.Router();
+const { readFile, writeFile } = fs;
 
+app.post('/create', async (req, res) => {
+    try {
+        let account = req.body;
+        const accounts = JSON.parse(await readFile("accounts.json"));
 
-router.post('/create', (req, res) => {
-    
+        account = {
+            id: accounts.nextId,
+            ...account
+        };
+
+        accounts.nextId++
+
+        accounts.accounts.push(account);
+
+        await writeFile("accounts.json", JSON.stringify(accounts));
+
+        res.status(200).send({
+            success: true,
+            message: "Conta criada com sucesso !",
+            response: account
+        })
+
+    } catch (error) {
+        res.status(400).send({
+            success: false,
+            message: error.message
+        });
+    }
 });
 
-export default router;
+export default app;
